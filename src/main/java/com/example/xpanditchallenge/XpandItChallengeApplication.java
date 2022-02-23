62fe9795fe1397d40abdcfe5b1755b833d920960
FeroBecass <bdi.oliveira@campus.fct.unl.pt>
2022-02-23 12:38:09 +0000

    first commit

---
+++ src/main/java/com/example/xpanditchallenge/XpandItChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.xpanditchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpandItChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpandItChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xpanditchallenge/XpandItChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpanditchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpandItChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
