4d78941b72f1301024e10de9c611f0f71e3bb74c
Bernardo <bdi.oliveira@campus.fct.unl.pt>
2022-02-26 21:32:21 +0000

    remove target from github

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO movies (id, title, launch_Date, rank, revenue) VALUES
-    (50, 'Iron Man', '2008-05-01', 7.9, 140),
-    (51, 'Iron Man 2', '2010-04-29', 6.9, 623),
-    (52, 'The Avengers', '2012-04-25', 9.2, 1519);
